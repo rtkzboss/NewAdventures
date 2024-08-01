bfb102e29305c5b7ad3991a5bf8ae460b7fd6e40
rtkz <154375853+rtkzboss@users.noreply.github.com>
2024-07-31 23:58:06 -0500

    More

--- Scripts/generate-assets.ps1
+++ Scripts/generate-assets.ps1
@@ -2,10 +2,10 @@ Param
 (
   [parameter(mandatory=$false, ValueFromRemainingArguments=$true)]$remaining
 )
-E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss `
+E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss --and-deps --stub-materials --stub-bps --stub-anims --material-debug-allow-incomplete `
   -a /Game/00_Main/UI/MainMenu/UMG_UnlocksEquipmentScreen `
   -a /Game/00_Main/UI/Multiplayer/UMG_Multiplayer_JobsScreen `
   -a /Game/00_Main/UI/MainMenu/UMG_UnlocksHubScreen `
   -a /Game/00_Main/Core/UI/Menus/UMG_SystemMenu `
   -a /Game/00_Main/Core/UI/Menus/Settings/UMG_SettingsMenu `
-  --and-deps --stub-materials --stub-bps --material-debug-allow-incomplete @remaining
+  @remaining
