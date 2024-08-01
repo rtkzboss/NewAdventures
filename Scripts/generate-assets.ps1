59ff1e7d40d9495bb16dd7e77c06ec293b9a2a2d
rtkz <154375853+rtkzboss@users.noreply.github.com>
2024-07-31 19:37:03 -0500

    More work

---
+++ Scripts/generate-assets.ps1
@@ -0,0 +1,11 @@
+Param
+(
+  [parameter(mandatory=$false, ValueFromRemainingArguments=$true)]$remaining
+)
+E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss `
+  -a /Game/00_Main/UI/MainMenu/UMG_UnlocksEquipmentScreen `
+  -a /Game/00_Main/UI/Multiplayer/UMG_Multiplayer_JobsScreen `
+  -a /Game/00_Main/UI/MainMenu/UMG_UnlocksHubScreen `
+  -a /Game/00_Main/Core/UI/Menus/UMG_SystemMenu `
+  -a /Game/00_Main/Core/UI/Menus/Settings/UMG_SettingsMenu `
+  --and-deps --stub-materials --material-debug-allow-incomplete @remaining
