7f41d5ed67580ffdcedb9b7eff2a1e1ae6747728
rtkz <154375853+rtkzboss@users.noreply.github.com>
2024-08-21 02:14:38 -0500

    Much more

--- Scripts/generate-assets.ps1
+++ Scripts/generate-assets.ps1
@@ -2,7 +2,7 @@ Param
 (
   [parameter(mandatory=$false, ValueFromRemainingArguments=$true)]$remaining
 )
-E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss --and-deps --stub-materials --stub-bps --stub-anims --material-debug-allow-incomplete `
+E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss --and-deps --stub-materials --stub-bps --stub-anims --material-debug-allow-incomplete --no-default-script --script E:\proj\hyde2\extract\res\editor-script.json --script "C:\Program Files (x86)\Steam\steamapps\common\CrimeBossRockayCity\CrimeBoss\Binaries\Win64\hyde\script.json" `
   -a /Game/00_Main/UI/MainMenu/UMG_UnlocksEquipmentScreen `
   -a /Game/00_Main/UI/Multiplayer/UMG_Multiplayer_JobsScreen `
   -a /Game/00_Main/UI/MainMenu/UMG_UnlocksHubScreen `
@@ -14,4 +14,6 @@ E:\proj\hyde2\target\release\extract --uncooked --steam -o E:\proj\CrimeBoss --a
   -a /Game/00_Main/Weapons/DT_WeaponSkinDatabase `
   -a /Game/00_Main/Core/AI/Settings/DA_AI_Database_FPS `
   -a /Game/00_Main/Core/Tables/DA_FPSDifficulty `
+  -a /Game/00_Main/UI/HUD/UMG_HUD_New `
+  -a /Game/00_Main/Core/HUD/BP_HUD `
   @remaining
